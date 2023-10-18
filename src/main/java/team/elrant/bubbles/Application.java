e2e15ea4e87128a10434691bdf50d161074ed5bb
Iris Nkrichronos <nyaambxr@elrant.team>
2023-10-18 12:22:43 +0200

    :D

--- src/main/java/team/elrant/bubbles/Application.java
+++ src/main/java/team/elrant/bubbles/Application.java
@@ -7,17 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The entry point of the Spring Boot application.
- *
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  *
  */
 @SpringBootApplication
-@Theme(value = "my-app")
+@Theme(value = "Bubbles")
 public class Application implements AppShellConfigurator {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
 }
+
